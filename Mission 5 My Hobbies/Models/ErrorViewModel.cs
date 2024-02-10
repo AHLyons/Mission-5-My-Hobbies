394584572b454633a83f45c228c8f8426d823ae6
Adam Lyons <AdamLyons.BYU@gmail.com>
2024-02-09 18:39:48 -0700

    Add project files.

---
+++ Mission 5 My Hobbies/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mission_5_My_Hobbies.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
